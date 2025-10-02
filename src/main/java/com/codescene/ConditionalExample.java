916e776dd5e440e220fa77d273514388aadd974b
Adam Tornhill <adam@adamtornhill.com>
2025-10-02 09:57:51 +0200

    Add documentation to the source code in order to demonstrate modifications to non-exectuable lines of code

--- src/main/java/com/codescene/ConditionalExample.java
+++ src/main/java/com/codescene/ConditionalExample.java
@@ -2,8 +2,18 @@ package com.codescene;
 
 public class ConditionalExample {
 
+    /**
+     * This is an example on documentation. Modifying docs should never impact 
+     * coverage: docs are non-executable lines.
+     * @param num (a great name for an argument, isn't it?)
+     * @return Nothing. This method is all about side-effects.
+     */ */
     public void checkNumber(int num) {
         if (num > 0) {
+            // Let's add a comment inside the method body.
+            // We'll use this comment block as a modification target in 
+            // various PRs to illustrate how comments are filtered out from 
+            // the coverage checks.
             System.out.println("The number is positive");
         } else if (num < 0) {
             System.out.println("The number is negative");
