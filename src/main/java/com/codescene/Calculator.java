70e027d96da6fe2959c69f3801b66b4c994bbbba
Juraj Martinka <jumarko@gmail.com>
2025-10-15 11:58:50 +0200

    Test pr check

--- src/main/java/com/codescene/Calculator.java
+++ src/main/java/com/codescene/Calculator.java
@@ -37,4 +37,7 @@ public class Calculator {
     private double add(double firstOperand, double secondOperand) {
         return firstOperand + secondOperand;
     }
+    private double untested(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
 }
